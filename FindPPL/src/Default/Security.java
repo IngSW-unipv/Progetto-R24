d3b6d3eadca4d6b8c1b60a9ffd61ce0efeaa63c5
ElianP00 <157394191+ElianP00@users.noreply.github.com>
2025-02-27 21:26:53 +0100

    Delete FindPPL directory

--- FindPPL/src/Default/Security.java
+++
@@ -1,9 +0,0 @@
-package Default;
-
-public class Security {
-	
-    public boolean verifyPassword(String inputPassword, String actualPassword) {
-        // Logica per verificare una password
-        return inputPassword.equals(actualPassword);
-    }
-}
